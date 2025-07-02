950d2bc9708c0cd321204ea816954e2b5860c021
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-02 16:38:53 +0200

    second counter done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import Counter from "./components/Counter/Counter";
 function App() {
   return (
     <>
+      {/* By default: starts at 0, step = 0 */}
       <Counter />
+
+      {/* Starts at 10, steps of 2 */}
+      <Counter start={10} step={2} />
     </>
   );
 }
